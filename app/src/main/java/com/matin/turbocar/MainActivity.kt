d622a9137c74562101dc8d946fe16a4c0851e482
Yousef <matinfard.y@gmail.com>
2024-06-20 15:52:36 +0200

    initial commit

---
+++ app/src/main/java/com/matin/turbocar/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.matin.turbocar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.matin.turbocar.ui.theme.TurbocarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TurbocarTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Main(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Main(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TurbocarTheme {
+        Main()
+    }
+}
