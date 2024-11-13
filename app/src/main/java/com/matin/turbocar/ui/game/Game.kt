ca095121dfa8e6cfcf7d8b1867fecdaa8a5d0a8f
Yousef <matinfard.y@gmail.com>
2024-11-13 18:53:06 +0100

    Add manual navigation

--- app/src/main/java/com/matin/turbocar/ui/game/Game.kt
+++ app/src/main/java/com/matin/turbocar/ui/game/Game.kt
@@ -3,6 +3,7 @@ package com.matin.turbocar.ui.game
 import Background
 import android.media.MediaPlayer
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
@@ -13,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.matin.turbocar.R
+import com.matin.turbocar.Screen
 import com.matin.turbocar.ui.game.component.Block
 import com.matin.turbocar.ui.game.component.GameMenu
 import com.matin.turbocar.ui.game.component.Player
@@ -22,7 +24,7 @@ import com.matin.turbocar.ui.game.engine.gameCoroutineScope
 import com.matin.turbocar.ui.game.model.ViewPort
 
 @Composable
-fun Game(modifier: Modifier = Modifier) {
+fun Game(modifier: Modifier = Modifier, onMenuClick: (Screen) -> Unit = {}) {
     val context = LocalContext.current
     val mediaPlayer = remember {
         MediaPlayer.create(context, R.raw.racing).apply {
@@ -31,7 +33,7 @@ fun Game(modifier: Modifier = Modifier) {
         }
     }
 
-    BoxWithConstraints(modifier = modifier) {
+    BoxWithConstraints(modifier = modifier.systemBarsPadding()) {
         val viewPort = ViewPort(maxWidth.toPx(), maxHeight.toPx())
         val gameDi = GameDi.rememberGameDi(viewPort = viewPort, scope = gameCoroutineScope())
         val player = gameDi.playerLogic.player.collectAsState()
